fb85142d2f0de5ad6d8bc063cd28da38624e94fb
João Marcos Batista Cerqueira <joaombcerqueira@gmail.com>
2022-02-10 11:31:47 -0300

    Change Transporter

--- index.js
+++ index.js
@@ -10,17 +10,14 @@ const transporter = nodemailer.createTransport({
     host: 'smtp-mail.outlook.com',
     port: 587,
     secure: false,
-    auth: auth,
-    tls: {
-        rejectUnauthorized: false
-     }
+    auth: auth
 })
 
 
 var mailOptions = {
     from: process.env.EMAILFROM,
     to: process.env.EMAILTO,
-    subject: 'Teste Piru',
+    subject: 'Teste Piru 2',
     text: 'GULGULULULULULU'
 }
 
