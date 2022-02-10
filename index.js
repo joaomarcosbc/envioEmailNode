fd4018c26bd4503d5d7f3ba9db70220bc7f16b19
João Marcos Batista Cerqueira <jmbcerqueira@grupoprimavera.med.br>
2022-02-10 11:16:58 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer')
+require('dotenv').config()
+
+var auth = {
+    user: process.env.EMAILFROM,
+    pass: process.env.PASSWORD
+}
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp-mail.outlook.com',
+    port: 587,
+    secure: false,
+    auth: auth,
+    tls: {
+        rejectUnauthorized: false
+     }
+})
+
+
+var mailOptions = {
+    from: process.env.EMAILFROM,
+    to: process.env.EMAILTO,
+    subject: 'Teste Piru',
+    text: 'GULGULULULULULU'
+}
+
+transporter.sendMail(mailOptions, (error, info) => {
+    if(error) {
+        console.log(error)
+    }else {
+        console.log(`Email sent: ${info.response}`)
+    }
+})
